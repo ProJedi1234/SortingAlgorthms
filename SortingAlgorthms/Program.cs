e903f8f55eb5edf2b89d99216c632388e9339840
Aditya Dhar <adhar2032@gmail.com>
2021-11-25 12:23:06 -0800

    Created MergeSort

--- SortingAlgorthms/Program.cs
+++ SortingAlgorthms/Program.cs
@@ -6,7 +6,20 @@ namespace SortingAlgorthms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(algorithm1(new int[]{ 12, 11, 13, 5, 6, 7 }, 3));
+        }
+        static int algorithm1(int[] array, int k) {
+            MergeSort ob = new MergeSort(array);
+            ob.sort();
+
+            return array[k - 1];
+        }
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
         }
     }
 }
